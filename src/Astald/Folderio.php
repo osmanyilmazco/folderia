c21da8c6d8c8c1aea7c71cb63ca7788c27f2468b
Osman YILMAZ <osmanyilmaz299@aof.anadolu.edu.tr>
2016-03-25 11:54:24 +0200

    Update Folderio.php
    Update version 1.0.1

--- src/Astald/Folderio.php
+++ src/Astald/Folderio.php
@@ -1,19 +1,21 @@
 <?php 
 
-/*
+/**
  * Folderio, Klasör sınıfı
  *  
  * Author:  Osman YILMAZ 
  * Email:   osmnylmz@outlook.com
  * Web:     http://www.astald.com - www.osmnylmz.com
  * 
- * Created Date: 23.03.2016
- *
+ * Created Date:	23.03.2016
+ * Update Date:		25.03.2016
 **/
 
 namespace Astald;
 
 use Astald\FieldSortHeap;
+use Exception;
+use DirectoryIterator;
 use RecursiveDirectoryIterator;
 use RecursiveIteratorIterator;
 
@@ -22,38 +24,99 @@ class Folderio
 	/*
 	 * Class is version 
 	*/
-	const VERSION = '1.0';
+	const VERSION = '1.0.5';
 
-	/*
-	 * Class is variable
+	/**
+	 * Hata çıktı sonucu
+	 * @var boolean
 	*/
 	public $debug = false;
-	public $rootFolder = NULL;
-	public $rootFolderName = NULL;
-	public $orderBy = NULL;
+
+	/**
+	 * Kök dizin değişkeni
+	 * @var string
+	*/
+	public $rootFolder = null;	
+
+	/**
+	 * Kök dizin isim değişkeni
+	 * @var string
+	*/
+	public $rootFolderName = null;
+
+	/**
+	 * Listleme filtre değişkeni
+	 * @var string
+	*/
+	public $orderBy = null;
+
+	/**
+	 * Gizli dosya ve klasör değişkeni
+	 * @var array
+	*/
+	public $hiddenFiles = array('.','..');   
+
+	/**
+	 * Klasör alt kayıt değişkeni
+	 * @var boolean
+	*/
+	public $children = false;
+
+	/**
+	 * Klasör oluşturma değişkeni
+	 * @var string
+	*/
+	public $createFolder = null;
+
+	/**
+	 * Klasör silme değişkeni
+	 * @var string
+	*/
+	public $deleteFolder = null;
+
+	/**
+	 * Klasör paket değişkeni
+	 * @var string
+	*/
 	public $packageFolder = 'localhost';
-	public $fileHidden = array('.','..');  
-	public $fileHiddenAdd = array();  
-	public $threeList = false;
 
-	/*
-	 *  Sınıf çağrılacağı zaman ilk çalışacak olan methodumuz.
-	**/
-	public function __construct() 
-	{ 
-		$this->rootFolder = $_SERVER["DOCUMENT_ROOT"];
-		
-		array_push($this->fileHidden, $this->packageFolder); 
-	}
 
-	/*
-	 *  gizlemek istediğimiz dosya ve klasörler için kullanmış olduğumuz method
-	**/
-	public function setFileHidden($fileHiddenAdd)
+	/**
+     * __construct methodu verileri atar.
+     *
+     * @param array $config
+     */
+	public function __construct(array $config = array()) 
 	{
-		$this->fileHidden = array_merge($this->fileHidden, $fileHiddenAdd); 
+		foreach ($config as $key => $value) {
+			$accept = array('debug', 'orderBy' , 'children');
+
+			if(isset($this->$key) and in_array($key, $accept)) {
+				$this->$key = $value;
+			}
+		}
+		$this->rootFolder = realpath($_SERVER["DOCUMENT_ROOT"]);
+
+		array_push($this->hiddenFiles, $this->packageFolder); 
 	}
 
+
+	/**
+     * Gizlenecek olan dosya, klasörleri belirler
+     *
+     * @param string $file
+     * @return array
+     */
+	public function setHiddenFiles($file)
+	{
+		$this->hiddenFiles = array_merge($this->hiddenFiles, $file); 
+	}
+
+	/**
+     * Kökdizinin belirler, varsayılan kök dizindir.
+     *
+     * @param string $rootFolderName
+     */
 	public function setFolderName($rootFolderName)
 	{
 		if(!empty($rootFolderName)) 
@@ -65,14 +128,18 @@ class Folderio
 			$this->rootFolder;
 	}
 
+	/**
+     * Klasör ve dosyaları listeler.
+     *
+     * @return array
+     */
 	public function getFolder()
 	{
-		try 
-		{
-			$this->rootFolderName = new RecursiveDirectoryIterator($this->rootFolder);
-			// $this->threeList // $this->rootFolderName = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($this->rootFolder), RecursiveIteratorIterator::SELF_FIRST); 
+		try {
+			$this->rootFolderName = new DirectoryIterator($this->rootFolder);
+			// $this->rootFolderName = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($this->rootFolder), RecursiveIteratorIterator::SELF_FIRST); 
 		} catch (RuntimeException $e) {
-			throw new Exception("Error Processing Request Error: {$e->getMessage()}", 1);
+			throw new Exception("Error Processing Request Error: {$e->getMessage()}");
 		}
 
 		if(!empty($this->orderBy)) 
@@ -81,30 +148,23 @@ class Folderio
 		}
 
 		foreach($this->rootFolderName as $fileinfo) 
-		{  
-			if(!in_array($fileinfo->getFileName(), $this->fileHidden)) 
-			{
-				if(!empty($this->orderBy))
-				{
-					$this->folderList->insert(array(
-						'name' => $fileinfo->getPathname(),
-						'fullname' => $fileinfo,
-						'filename' => $fileinfo->getFileName(),
-						'type' => ($fileinfo->getType() == "dir") ? "dir" : mime_content_type($fileinfo->getRealPath()),
-						'size' => $this->fileSizeConvert($fileinfo->getSize()),
-						'lastmod' => $fileinfo->getMTime()
-					));
-				}
-				else
-				{
-					$this->folderList[] = array(
-						'fullname' => $fileinfo,
-						'name' => $fileinfo->getPathname(),
-						'filename' => $fileinfo->getFileName(),
-						'type' => ($fileinfo->getType() == "dir") ? "dir" : mime_content_type($fileinfo->getRealPath()),
-						'size' => $this->fileSizeConvert($fileinfo->getSize()),
-						'lastmod' => $fileinfo->getMTime()
-					);
+		{   
+			if (!in_array($fileinfo->getFileName(), $this->hiddenFiles)) {
+				$fileArrays = array(
+					'name' => $fileinfo->getPathname(),
+					'fullName' => $fileinfo,
+					'fileName' => $fileinfo->getFileName(),
+					'type' => ($fileinfo->getType() == "dir") ? "dir" : "file",
+					'realType' => ($fileinfo->getType() == "dir") ? "dir" : mime_content_type($fileinfo->getRealPath()),
+					'size' => $this->fileSizeConvert($fileinfo->getSize()),
+					'visible' => $fileinfo->isDot() ? 1 : 0,
+					'getOwner' => $fileinfo->getOwner(),
+					'lastMod' => $fileinfo->getMTime()
+				);
+				if (!empty($this->orderBy)) {
+					$this->folderList->insert($fileArrays); 
+				} else {
+					$this->folderList[] = $fileArrays; 
 				}
 			}
 			 
@@ -114,27 +174,84 @@ class Folderio
 	}
 
 
+	/**
+     * Dosya ve Klasör varlığını kontrol eder.
+     *
+     * @param string $name
+     * @return boolean
+     */
+	public function folderOrFileExists($name) 
+	{
+		if (file_exists($name)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-	/*
-	 * File Size
-	*/ 
-	public $bytes = NULL;
+
+	/**
+     * Klasör oluşturur
+     *
+     * @param string $name
+     * @param integer $chmod
+     * @return boolean
+     */
+	public function createFolder($name, $chmod = 0700) 
+	{
+		try {
+			$this->createFolder = $this->rootFolder ."/". $name;
+
+			if ($this->folderOrFileExists($this->createFolder)) {
+				throw new Exception("File exists! Please change folder or file name");			
+			}
+			return mkdir($this->createFolder, $chmod); 
+		} catch (RuntimeException $e) {
+			throw new Exception("Couldn't create folder or file: {$e->getMessage()}");
+		}
+	}
+
+	/**
+     * Klasör siler
+     *
+     * @param string $name
+     * @param integer $chmod
+     * @return boolean
+     */
+	public function deleteFolder($name, $permission = true) 
+	{
+		try {
+			$this->deleteFolder = $this->rootFolder ."/". $name;
+
+			if (!$this->folderOrFileExists($this->deleteFolder)) {
+				throw new Exception("File doesn't exists!");			
+			}
+			return rmdir($this->deleteFolder); 
+		} catch (RuntimeException $e) {
+			throw new Exception("Couldn't deleted folder or file: {$e->getMessage()}");
+		}
+	}
+
+	/**
+     * Dosya boyutunu hesaplar
+     * 
+     * @param integer $bytes 
+     */
 	public function fileSizeConvert($bytes)
     {
-        if ($bytes >= 1073741824)
-            $this->bytes = number_format($bytes / 1073741824, 2) . ' GB';
-        elseif ($bytes >= 1048576)
-            $this->bytes = number_format($bytes / 1048576, 2) . ' MB';
-        elseif ($bytes >= 1024)
-            $this->bytes = number_format($bytes / 1024, 2) . ' KB';
-        elseif ($bytes > 1)
-            $this->bytes = $bytes . ' bytes';
-        elseif ($bytes == 1)
-            $this->bytes = $bytes . ' byte';
-        else
-            $this->bytes = '0 bytes';
-
-        return $this->bytes;
+        if ($bytes >= 1073741824) {
+            return number_format($bytes / 1073741824, 2) . ' GB';
+        } elseif ($bytes >= 1048576) {
+            return number_format($bytes / 1048576, 2) . ' MB';
+        } elseif ($bytes >= 1024) {
+            return number_format($bytes / 1024, 2) . ' KB';
+        } elseif ($bytes > 1) {
+            return $bytes . ' bytes';
+        } elseif ($bytes == 1) {
+            return $bytes . ' byte';
+        } else {
+            return '0 bytes';
+        }
 	}
 
 }
