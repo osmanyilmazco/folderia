3915dc77d61f46fa3c357c128dcecedbf5766d86
Osman YILMAZ <osmanyilmaz299@aof.anadolu.edu.tr>
2016-03-25 11:52:43 +0200

    Update example.php

--- example.php
+++ example.php
@@ -14,30 +14,39 @@
 // require_once __DIR__ . '/vendor/autoload.php';   // Composer ile kullanmak için.
 require_once __DIR__ . '/src/autoload.php';  // Composer olmadan kullanmak için.
 
+$config = array(
+	'debug'		=> 'name',
+	'orderBy'	=> 'name',
+);
+
 /*
  * Sınıfımızı tanımlıyoruz $folder = new Astald\Folderio; kullanımı yerine use ile sınıfımızda belirtiyoruz. Siz istediğiniz gibi kullanabilirsiniz.
 */
 use Astald\Folderio;
-$folder = new Folderio;
+$folder = new Folderio; // $folder = new Folderio($config);
 
-// Listeleme tipi varsayılan NULL dur, ihtiyacınıza göre, "size", "type" vb. yapabilirsiniz.
+$folder->setHiddenFiles(array("index.php", "zsystem", '127_0_0_1.sql'));
+
+// $folder->deleteFolder('testCan');
+
 $folder->orderBy = 'type';
 
-/* Listeleme yapılırken görünmesini istemediğimiz dosya veya klasörlerimizin adınız yazıyoruz. */
-$folder->setFileHidden(array("index.php","zsystem"));
+// $folder->createFolder('gurkanUzunca');
 
-/* setFolderName() fonksiyonumuza, listelemenin baz alınacağı klasörü yazıyoruz. Varsayılan kök dizindir. */
-$folder->setFolderName('customer');
+echo "<table border=1>";
+echo "<thead>";
 
+echo "<tr><th>Dosya Adı</th><th>Tam Dosya Adı:</th><th>Dosya Türü:</th><th>Dosya Boyutu:</th><th>Son Düzenleme Tarihi</th><th>Görünürlük</th></tr>";
+echo "</thead>";
 foreach ($folder->getFolder() as $key => $value) {
-	echo "Dosya Adı : {$value['filename']}";
-	echo "<br>";
-	echo "Tam Dosya Adı: {$value['name']}";
-	echo "<br>";
-	echo "Dosya Türü: {$value['type']}";
-	echo "<br>";
-	echo "Dosya Boyutu: {$value['size']}";
-	echo "<br>";
-	echo "Son Düzenleme Tarihi: {$value['lastmod']}";
-	echo "<br>";
+	echo "<tr>";
+	echo "<td>{$value['fileName']}</td>";
+	echo "<td>{$value['name']}</td>";
+	echo "<td>{$value['type']}</td>";
+	echo "<td>{$value['size']}</td>";
+	echo "<td>{$value['lastMod']}</td>";
+	echo "<td>{$value['visible']}</td>";
+	echo "</tr>";
 }
+
+echo "</table>";
